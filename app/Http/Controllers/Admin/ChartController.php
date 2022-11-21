9704da6a8564db46a4773edba2e05f13d5d4a424
shundahoy <shundahoy@gmail.com>
2022-11-21 22:16:48 +0900

    updated

--- app/Http/Controllers/Admin/ChartController.php
+++ app/Http/Controllers/Admin/ChartController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\ClientReview;
+use App\Models\Chart;
 use Illuminate\Http\Request;
 
-class ClientReviewController extends Controller
+class ChartController extends Controller
 {
     public function onAllSelect()
     {
-        $result = ClientReview::all();
+        $result = Chart::all();
         return $result;
     }
 }
