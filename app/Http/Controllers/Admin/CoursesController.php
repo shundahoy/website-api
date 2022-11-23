b388b0d1f49379feb7526d39863fb7871aad4573
shundahoy <shundahoy@gmail.com>
2022-11-23 20:34:11 +0900

    updated

--- app/Http/Controllers/Admin/CoursesController.php
+++ app/Http/Controllers/Admin/CoursesController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Courses;
 use Illuminate\Http\Request;
 
 class CoursesController extends Controller
 {
-    //
+    public function onAllSelect()
+    {
+        $result = Courses::all();
+        return $result;
+    }
+
+    public function onSelectFour()
+    {
+        $result = Courses::limit(4)->get();
+        return $result;
+    }
+
+    public function onSelectDetails(Request $request)
+    {
+        $id = $request->input('id');
+        $result = Courses::where('id', $id)->get();
+        return $result;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\ChartController;
 use App\Http\Controllers\Admin\ClientReviewController;
 use App\Http\Controllers\Admin\ContactController;
+use App\Http\Controllers\Admin\CoursesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,3 +15,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/chartdata', [ChartController::class, 'onAllSelect']);
 Route::get('/clientreview', [ClientReviewController::class, 'onAllSelect']);
 Route::post('/contactsend', [ContactController::class, 'onContactSend']);
+Route::get('/coursehome', [CoursesController::class, 'onSelectFour']);
+Route::get('/courseall', [CoursesController::class, 'onAllSelect']);
+Route::post('/coursedetail', [CoursesController::class, 'onSelectDetails']);
